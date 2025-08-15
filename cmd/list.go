4e2036f82781d33d70212d0d5b9e734adbc9f0e6
Mike Partelow <github@mikepartelow.com>
2025-08-14 21:12:52 -0700

    doc: inline help

--- cmd/list.go
+++ cmd/list.go
@@ -12,14 +12,8 @@ import (
 
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.NoArgs,
+	Short: "List and search recipes.",
+	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		catalog := cmd.Context().Value("catalog").(*sozzler.RecipeCatalog)
 		display := cmd.Context().Value("display").(display.Display)
--- cmd/show.go
+++ cmd/show.go
@@ -10,13 +10,8 @@ import (
 
 var showCmd = &cobra.Command{
 	Use:   "show <recipe name>",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Show a recipe.",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
